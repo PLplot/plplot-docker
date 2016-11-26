2cf425871b1da2c9da006d67cc414aa64da8be5c
Hazen Babcock <hbabcock@mac.com>
2016-11-26 12:28:43 -0500

    Add note about arch-linux builds

--- build_all.py
+++ build_all.py
@@ -2,7 +2,10 @@
 #
 # (re)-builds all of the docker images.
 #
-
+# Notes:
+#   (1) arch-linux will always do a complete re-build even if nothing has
+#       changed, not sure why.
+#
 import subprocess
 
 import image_names
@@ -10,6 +13,3 @@ import image_names
 for elt in image_names.names:
     cmd = ["docker", "build", "-t", elt[0], "./plplot/" + elt[1] + "/."]
     subprocess.call(cmd)
-
-
-
