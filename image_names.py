6209b674bc2054874d6808f5d696557f4c15b447
Hazen Babcock <hbabcock@mac.com>
2016-11-26 12:28:08 -0500

    Add Python tests

--- image_names.py
+++ image_names.py
@@ -3,11 +3,18 @@
 # Image names and directories as a list.
 #
 
-names = [["plplot/arch-linux", "arch_linux"],
-         ["plplot/centos-latest", "centos_latest"],
-         ["plplot/debian-latest", "debian_latest"],
-         ["plplot/debian-stable", "debian_stable"],
-         ["plplot/fedora-latest", "fedora_latest"],
-         ["plplot/opensuse-tumbleweed", "opensuse_tumbleweed"],
-         ["plplot/ubuntu-latest", "ubuntu_latest"]]
+# Distro test images.
+distro_test = [["plplot/arch-linux", "arch_linux"],
+               ["plplot/centos-latest", "centos_latest"],
+               ["plplot/debian-latest", "debian_latest"],
+               ["plplot/debian-stable", "debian_stable"],
+               ["plplot/fedora-latest", "fedora_latest"],
+               ["plplot/opensuse-tumbleweed", "opensuse_tumbleweed"],
+               ["plplot/ubuntu-latest", "ubuntu_latest"]]
 
+# Python test images.
+python_test = [["plplot/debian-latest-python27", "debian_latest_python27"],
+               ["plplot/debian-latest-python34", "debian_latest_python34"]]
+
+# All the test images.
+names = distro_test + python_test
